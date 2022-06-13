0031d2bf4a4428dd5cd6c16caedae6d3360803e9
TStorm <techstormng@gmail.com>
2022-06-13 19:03:26 +0100

    Include playRound function

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 function computerPlay(){
     let options = ['Rock','Paper','Scissors'];
-    let result = options[Math.floor(Math.random() * options.length)];
-    console.log(result);
+    let outcome = options[Math.floor(Math.random() * options.length)];
+    return outcome;
 }
 
-computerPlay();
+function playRound(playerChoice, computerChoice){
+  let result = '';
+
+  if (playerChoice ==="Rock"){
+      if(computerChoice === 'Paper'){
+          result = 'You lose! Paper beats Rock';
+      }else if(computerChoice === 'Scissors'){
+          result = "You win! Rock beats Scissors";
+      }else{
+          result = "It's a tie";
+      }
+  }else if(playerChoice ==="Paper"){
+      if(computerChoice === 'Scissors'){
+          result = 'You lose! Scissors beats Paper';
+      }else if(computerChoice === 'Rock'){
+          result = "You win! Paper beats Rock";
+      }else{
+          result = "It's a tie";
+      }
+  }else if(playerChoice === 'Scissors'){
+      if(computerChoice === 'Rock'){
+          result = 'You lose! Rock beats Scissors';
+      }else if(computerChoice === 'Paper'){
+          result = "You win! Scissors beats Paper";
+      }else{
+          result = "It's a tie";
+      }
+  }
+
+  return result;
+
+}
+
+const playerChoice = 'Rock';
+const computerChoice = computerPlay();
+console.log(playRound(playerChoice, computerChoice));
