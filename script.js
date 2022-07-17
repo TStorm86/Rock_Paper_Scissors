c664c18333c7c0fd317d42bed86b197066ae14fd
TStorm <techstormng@gmail.com>
2022-07-17 21:36:37 +0100

    Update game function, with user input validation.

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-function computerPlay(){
-    let options = ['Rock','Paper','Scissors'];
-    let outcome = options[Math.floor(Math.random() * options.length)];
-    return outcome;
+const choices = ["rock", "paper", "scissors"];
+
+function game(){
+    for (let i = 0; i <= 5; i++){
+        playRound();
+    }
+}
+
+
+function playRound(){
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+}
+
+
+function playerChoice(){
+    let input = prompt("Enter your option");
+    while(input == null){
+        input = prompt("Please enter a valid option");
+    }
+    input = (input.toLowerCase()).trim();
+    let check = validateInput(input);
+    while (check == false){
+        input = prompt("Check Spelling, Capitalization doesn't matter");
+        while(input == null){
+        input = prompt("Please enter a valid option");
+    }
+        input = (input.toLowerCase()).trim();
+        check = validateInput(input);
+    }
+    return input;
+}
+
+
+function computerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+
+function validateInput(choice){
+    if (choices.includes(choice)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function checkWinner(pChoice, cChoice){
+    console.log(pChoice, cChoice, '-----' );
+    if(pChoice === cChoice){
+        return "Its a tie";
+    }else if(
+        (pChoice === 'rock' && cChoice == 'scissors') ||
+        (pChoice === 'paper' && cChoice == 'rock') || 
+        (pChoice === 'scissors' && cChoice == 'paper')
+    ){
+        return "You win";
+    }else{
+        return "Computer wins"
+    }
 }
 
 
 
 
-function playRound(playerChoice, computerChoice){
-  let result = '';
-
-  if (playerChoice ==="ROCK"){
-      if(computerChoice === 'Paper'){
-          result = 'You lose! Paper beats Rock';
-      }else if(computerChoice === 'Scissors'){
-          result = "You win! Rock beats Scissors";
-      }else{
-          result = "It's a tie";
-      }
-  }else if(playerChoice ==="PAPER"){
-      if(computerChoice === 'Scissors'){
-          result = 'You lose! Scissors beats Paper';
-      }else if(computerChoice === 'Rock'){
-          result = "You win! Paper beats Rock";
-      }else{
-          result = "It's a tie";
-      }
-  }else if(playerChoice === 'SCISSORS'){
-      if(computerChoice === 'Rock'){
-          result = 'You lose! Rock beats Scissors';
-      }else if(computerChoice === 'Paper'){
-          result = "You win! Scissors beats Paper";
-      }else{
-          result = "It's a tie";
-      }
-  }else{
-      result = 'Invalid choice';
-  }
-
-  return result;
-
-}
-
- playerChoice = prompt("Enter choice:", "").toUpperCase();
- computerChoice = computerPlay();
-console.log(playRound(playerChoice, computerChoice));   
+game();
